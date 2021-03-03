7d9244ce9c7e4e563cba0608fd234cc2aeae0007
Sara Reed <reedsara90@gmail.com>
2021-03-02 22:18:30 -0700

    Saras Toy Problems

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import TopicBrowser from './components/TopicBrowser/TopicBrowser'      //import TopicBrowser AFTER the import of react
+
 
 class App extends Component {
   render() {
-    
+    return (
+      <TopicBrowser />
+    )
   }
 }
 
